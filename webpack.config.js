3240deb691617b0da49a1dd1beb4c171daaf344c
Matthew Grill <matt.grill@acquia.com>
2018-01-02 13:44:30 -0800

    travis

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const productionPluginDefine = process.env.NODE_ENV === 'production' ? [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production'),
-  }),
-  new webpack.optimize.UglifyJsPlugin(),
-] : [];
+const productionPluginDefine =
+  process.env.NODE_ENV === 'production'
+    ? [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
+      ]
+    : [];
 
 module.exports = [
   {
@@ -40,7 +43,8 @@ module.exports = [
             presets: [
               '@babel/preset-react',
               [
-                '@babel/preset-env', {
+                '@babel/preset-env',
+                {
                   modules: false,
                   targets: {
                     browsers: ['last 2 versions'],
