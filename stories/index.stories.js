d91dfb19b05d6d8159c44c2b9698af7c94ef6d59
Daniel Wehner <dawehner@googlemail.com>
2018-01-12 19:08:12 +0000

    Fix eslint failures

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
 import Select from '../src/components/select';
 import { Breadcrumb, BreadcrumbItem } from '../src/components/breadcrumb';
